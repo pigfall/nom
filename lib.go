a919d6205ad800f27d55808ac8c5f29638d807be
tzz <koi_town@outlook.com>
2022-06-04 15:19:20 +0800

    lib.go: alpha0,aplha1

--- lib.go
+++ lib.go
@@ -3,6 +3,7 @@ package nom
 import(
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 func Tag(tag string)ParseFn{
@@ -15,5 +16,41 @@ func Tag(tag string)ParseFn{
 		}
 
 		return IResult{},parseErr(fmt.Sprintf("tag [%s]not match",tag)).notMatch().build()
+	}
+}
+
+func Alpha0()ParseFn{
+	return func(input string)(IResult,error){
+		if len(input) == 0{
+			return IResult{
+				notParsed:input,
+			},nil
+		}
+
+		for i,r := range []rune(input){
+			if unicode.IsLetter(r){
+				continue
+			}else{
+				return IResult{
+					notParsed:string([]rune(input)[i:]),
+					produced:string([]rune(input)[:i]),
+				},nil
+			}
+		}
+
+		panic("unreachable")
+	}
+}
+
+func Alpha1()ParseFn{
+	return func(input string)(IResult,error){
+		i,err :=Alpha0()(input)
+		if err != nil{
+			return IResult{},err
+		}
+		if len(i.produced) == 0{
+			return IResult{notParsed:input},parseErr("need at least one alpha").notMatch().build()
+		}
+		return i,nil
 	}
 }
--- lib_test.go
+++ lib_test.go
@@ -34,3 +34,78 @@ func TestTag(t *testing.T){
 		test.want(&r,e)
 	}
 }
+
+func TestAlpha0(t *testing.T){
+	tests := []struct{
+		tag string
+		input string
+		want func(result *IResult,err error)
+	}{
+		{
+			input:"tzz33",
+			want:func(result *IResult,err error){
+				require.Equal(t,nil,err)
+				require.Equal(t,"tzz",result.produced)
+				require.Equal(t,"33",result.notParsed)
+			},
+		},
+		{
+			input:"t2zz33",
+			want:func(result *IResult,err error){
+				require.Equal(t,nil,err)
+				require.Equal(t,"2zz33",result.notParsed)
+				require.Equal(t,"t",result.produced)
+			},
+		},
+		{
+			input:" t2zz33",
+			want:func(result *IResult,err error){
+				require.Equal(t,nil,err)
+				require.Equal(t," t2zz33",result.notParsed)
+				require.Equal(t,"",result.produced)
+			},
+		},
+	}
+
+	for _,test := range tests{
+		var  r,e = Alpha0()(test.input)
+		test.want(&r,e)
+	}
+}
+
+func TestAlpha1(t *testing.T){
+	tests := []struct{
+		tag string
+		input string
+		want func(result *IResult,err error)
+	}{
+		{
+			input:"tzz33",
+			want:func(result *IResult,err error){
+				require.Equal(t,nil,err)
+				require.Equal(t,"tzz",result.produced)
+				require.Equal(t,"33",result.notParsed)
+			},
+		},
+		{
+			input:"t2zz33",
+			want:func(result *IResult,err error){
+				require.Equal(t,nil,err)
+				require.Equal(t,"2zz33",result.notParsed)
+				require.Equal(t,"t",result.produced)
+			},
+		},
+		{
+			input:" t2zz33",
+			want:func(result *IResult,err error){
+				require.Equal(t,true,MustParseErr(err).IsNotMatch())
+				require.Equal(t," t2zz33",result.notParsed)
+			},
+		},
+	}
+
+	for _,test := range tests{
+		var  r,e = Alpha1()(test.input)
+		test.want(&r,e)
+	}
+}
